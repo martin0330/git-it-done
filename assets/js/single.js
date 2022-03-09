40205f713adcc37dc3d3253694aad1166ba06ffa
Martin cruz <martin@Martins-MacBook-Air.local>
2022-03-08 22:30:18 -0600

    added issues and message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 
 var getRepoIssues = function(repo) {
@@ -10,14 +11,17 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
         }
     });
     };
-
-    getRepoIssues("facebook/react");
 
 var displayIssues = function(issues) {
 
@@ -33,28 +37,42 @@ var displayIssues = function(issues) {
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
 
-        issueContainerEl.appendChild(issueEl);
-    }
-
-        // create span to hold issue title
+         // create span to hold issue title
         var titleEl = document.createElement("span");
         titleEl.textContent = issues[i].title;
-    
-        //append to container
+
+         //append to container
         issueEl.appendChild(titleEl);
     
-        // create a type element
+         // create a type element
         var typeEl = document.createElement("span")
-    
-        // check if issue is an actual issue or a pull request
+     
+         // check if issue is an actual issue or a pull request
         if (issues[i].pull_request) {
             typeEl.textContent = "(Pull request)";
         } else {
             typeEl.textContent = "(Issue)";
         }
-    
-        // append to container
+     
+         // append to container
         issueEl.appendChild(typeEl);
-    
+     
 
+        issueContainerEl.appendChild(issueEl);
+    }
 };
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
